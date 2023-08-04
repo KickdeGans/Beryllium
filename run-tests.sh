e1d59352c55aca5920b5aa4e2c0d14fbcad6521a
Kick de Gans <95710967+KickdeGans@users.noreply.github.com>
2023-08-04 10:54:35 +0200

    Delete run-tests.sh

--- run-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This file runs ALL tests
-
-./fusion tests/for.fn
-./fusion tests/foreach.fn
-./fusion tests/if.fn
-./fusion tests/import.fn
-echo "Hello" | ./fusion tests/input.fn
-./fusion tests/print.fn
