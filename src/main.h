bdd0121ff37dfb7b69afe27f0d9bb61147351f10
mrtianjin829 <100748047+mrtianjin829@users.noreply.github.com>
2023-08-06 23:17:38 +0530

    Simplify argument processing using macros

--- src/main.h
+++ src/main.h
@@ -1,4 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define FCARG(sw,flag) \
+	if(fast_compare(argv[i],sw)==0)
+
+#define FCARG_ACTION(flag) \
+	{ \
+            flag = 1; \
+            i++; \
+            compile_file = argv[i]; \
+            break; \
+        }
+
 #endif
